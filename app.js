2134e393005c936d50d2db0b48714a5f7e03785e
Augusto Schmidt <augusto.lampert.schmidt@gmail.com>
2024-05-28 18:18:54 -0300

    voltar ao normal

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ function exibirTextoNaTela(tag, texto) {
 
 function mensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número de 1 a 10 teste');
+    exibirTextoNaTela('p', 'Escolha um número de 1 a 10');
 }
 
 function limparCampo() {
